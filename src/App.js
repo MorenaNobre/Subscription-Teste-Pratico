11856a0e4658c62ae6f3c97293e9086b90b156e5
MorenaNobre <morenagnobre@gmail.com>
2021-06-25 12:18:43 -0300

    added env file and cart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import React from 'react'
-// import Navbar from './components/Navbar/Navbar'
-// import Products from './components/Products/Products'
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
 
-import { Products, Navbar } from './components'
+import { Products, Navbar } from "./components";
+
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ["DM Sans"].join(","),
+  },
+});
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Products />
-    </div>
-  )
-}
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
-export default App
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  console.log(cart);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <Navbar />
+        <Products products={products} onAddToCart={handleAddToCart} />
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default App;
